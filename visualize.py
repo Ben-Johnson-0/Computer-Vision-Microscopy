b9942290d9c5aa51d7e55c9f6327eec698f0426a
Ben-Johnson-0 <143573847+Ben-Johnson-0@users.noreply.github.com>
2023-10-23 22:47:21 -0400

    Simple 2D data visualization

---
+++ visualize.py
@@ -0,0 +1,22 @@
+# Quick, simple visualization of the training data for the 2D data
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# fp = "./data/Denoising_Tribolium/train_data/data_label.npz"
+fp = "./data/Synthetic_tubulin_gfp/train_data/data_label.npz"
+data = np.load(fp)
+
+print('X', data['X'].shape)
+print('Y', data['Y'].shape)
+
+for i in range(data['X'].shape[0]):
+    img1 = np.squeeze(data['X'][i])
+    img2 = np.squeeze(data['Y'][i])
+    plt.clf()
+    plt.subplot(211)
+    plt.imshow(img1)
+    plt.title(f"Image {i}")
+    plt.subplot(212)
+    plt.imshow(img2)
+    plt.show()
