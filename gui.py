abefae9a031d2108a38bca24b09b6b9e7579cf53
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2023-12-07 00:55:53 -0500

    Add simple model gui

---
+++ gui.py
@@ -0,0 +1,122 @@
+import PySimpleGUI as sg
+import os.path
+from PIL import Image, ImageTk
+import io
+import torch
+from torchvision.transforms import ToTensor
+from torchvision.utils import save_image
+from utils import tensor_to_patches, patches_to_tensor
+
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+# https://www.blog.pythonlibrary.org/2021/02/16/creating-an-image-viewer-with-pysimplegui/
+# Need this so we can open and view tiffs
+def get_image_data(f, maxsize=(1920, 1080)):
+    """ Generate Image data using PIL and io to get data """
+    image = Image.open(f)
+    image.thumbnail(maxsize)
+    bio = io.BytesIO()
+    image.save(bio, format="PNG")
+    return bio.getvalue()
+
+sg.theme('DarkAmber')
+left_column = [
+    [
+        sg.Text("Model File"),
+        sg.In(size=(25,1), enable_events=True, key="-MODEL-"),
+        sg.FileBrowse(),
+    ],
+    [
+        sg.Text("Input Image File"),
+        sg.In(size=(25,1), enable_events=True, key="-INP PATH-"),
+        sg.FileBrowse(),
+    ],
+    [
+        sg.Text("Output Image File"),
+        sg.In(size=(25,1), enable_events=True, key="-OUT PATH-"),
+        sg.Button("Save")
+    ],
+    [
+        sg.Button("Denoise"),
+        sg.Text("", key="-ERROR MSG-")
+    ],
+]
+
+image_column = [
+    [sg.Text("Chosen input image:")],
+    [sg.Text(size=(60, 1), key="-INP TITLE-")],
+    [sg.Image(key="-INP IMG-")],
+    [sg.HSeparator()],
+    [sg.Text("Output image:")],
+    [sg.Text(size=(60, 1), key="-OUT TITLE-")],
+    [sg.Image(key="-OUT IMG-")],
+]
+
+layout = [
+    [
+        sg.Column(left_column),
+        sg.VSeperator(),
+        sg.Column(image_column),
+    ]
+]
+
+window = sg.Window("Image Denoiser", layout)
+model = None
+inp_img = None
+out_path = None
+
+# Event loop
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    
+    if event == "-MODEL-":
+        model_path = values["-MODEL-"]
+        model = torch.load(model_path)
+
+    if event == "-INP PATH-":
+        try:
+            inp_img = values["-INP PATH-"]
+            window["-INP TITLE-"].update(inp_img)
+            window["-INP IMG-"].update(data=get_image_data(inp_img))
+        except:
+            pass
+
+    if event == "-OUT PATH-":
+        out_path = values["-OUT PATH-"]
+
+    if event == "Denoise":
+        if inp_img == None:
+            window["-ERROR MSG-"].update("Missing input image file.", text_color="red")
+            continue
+        if model == None:
+            window["-ERROR MSG-"].update("Missing model file.", text_color="red")
+            continue
+        if out_path == None:
+            out_path = os.path.join(''.join(inp_img.split("\/")[:-1]), "out.png")
+            window["-ERROR MSG-"].update(f"Saving output to \"{out_path}\".", text_color="yellow")
+
+        if not out_path.endswith(".png"):
+            out_path += ".png"
+
+        # Open image and save its original dimensions
+        inp_tensor = Image.open(inp_img)
+        inp_tensor = ToTensor()(inp_tensor)
+        inp_tensor = torch.unsqueeze(inp_tensor, dim=0)
+        original_dims = inp_tensor.shape
+
+        # Modify tensor for being input to the model
+        inp_tensor = torch.sigmoid(inp_tensor).float().to(DEVICE)
+        inp_tensor = tensor_to_patches(inp_tensor, 64)
+        # inp_tensor = torch.reshape(inp_tensor, (-1,1,64,64))
+
+        # Get output, return it from the patches
+        out_image = model(inp_tensor)
+        out_image = patches_to_tensor(out_image, original_dims, 64)
+        # out_image = torch.reshape(out_image, original_dims)
+        save_image(out_image, out_path)
+        window["-OUT IMG-"].update(filename=out_path)
+        window["-OUT TITLE-"].update(out_path)
+
+window.close()
--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ def save_img(img, title, file_name, num_rows, num_cols):
     plt.title(title)
     plt.savefig(file_name)
 
-# Apply FFT then iFFT to denoise an image/stack of images
 def fft_denoise(image_tensor, ratio=0.1):
+    """ Apply FFT then iFFT to denoise an image/stack of images """
     image_fft = torch.fft.fft2(image_tensor, dim=(-2, -1))
     
     magnitude = torch.abs(image_fft)
@@ -21,3 +21,40 @@ def fft_denoise(image_tensor, ratio=0.1):
     # Step 3: Apply inverse FFT to obtain the denoised image
     denoised_image = torch.fft.ifft2(image_fft, dim=(-2, -1)).real
     return denoised_image
+
+
+def tensor_to_patches(input_tensor, patch_size):
+    """ Generate Patches from a 4D tensor """
+    batch_size, channels, height, width = input_tensor.size()
+    patches = input_tensor.unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
+    # patches shape: [1, 1, 4, 8, 64, 64]
+    patches = patches.contiguous().view(batch_size, channels, -1, patch_size, patch_size)
+    # patches shape: [1, 1, 32, 64, 64]
+    patches = patches.squeeze(dim=0)
+    # patches shape: [1, 32, 64, 64]
+    patches = patches.permute(1, 0, 2, 3).contiguous().view(-1, channels, patch_size, patch_size)
+    # patches shape: [32, 1, 64, 64]
+
+    return patches
+
+def patches_to_tensor(patches, original_size, patch_size):
+    """ Return a patched tensor to its original 4D shape """
+    # original size: [1,1,256,512]
+    batch_size, channels, height, width = original_size
+
+    patches = patches.view(batch_size, channels, -1, patch_size, patch_size)
+    # patches shape: [32, 1, 64, 64]
+    patches = patches.permute(0, 2, 1, 3, 4).contiguous().view(batch_size, channels, -1, patch_size, patch_size)
+    # patches shape: [1, 1, 32, 64, 64]
+
+    unfolded_height = height // patch_size  # 4
+    unfolded_width = width // patch_size    # 8
+    
+    reconstructed = patches.view(batch_size, channels, unfolded_height, unfolded_width, patch_size, patch_size)
+    # rec shape: [1, 1, 4, 8, 64, 64] 
+    reconstructed = reconstructed.permute(0, 1, 2, 4, 3, 5).contiguous()
+    # rec shape: [1, 1, 4, 64, 8, 64]
+    reconstructed = reconstructed.view(batch_size, channels, unfolded_height * patch_size, unfolded_width * patch_size)
+    # rec shape: [1, 1, 4*64, 8*64] == [1, 1, 256, 512]
+
+    return reconstructed
